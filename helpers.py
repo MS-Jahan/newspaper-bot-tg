5d22d7701680f91246d2fd828f5abac9a298aced
ms-jahan <contact@sabit.dev>
2024-11-20 16:17:58 +0600

    Enhance proxy checking by adding HTML validation and integrating proxyscrape for proxy retrieval

--- helpers.py
+++ helpers.py
@@ -5,6 +5,10 @@ from bs4 import BeautifulSoup
 from multiprocessing.pool import ThreadPool
 import traceback
 
+headers = {
+    "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36"
+}
+
 def check_nu_working():
     URL = f"https://hc-ping.com/{os.getenv('HC_PING_ID')}"
     r = requests.get(URL)
@@ -35,52 +39,50 @@ def get_socks5_proxy():
 def check_proxies(proxy_arr):
     def is_proxy_working(proxy):
         try:
-            response = requests.get("https://httpbin.org/ip", proxies={"http": proxy, "https": proxy}, timeout=5)
-            return response.status_code == 200
+            response = requests.get("https://www.nu.ac.bd/recent-news-notice.php", headers=headers, proxies={"http": proxy, "https": proxy}, timeout=5)
+            if response.status_code == 200:
+                return response.text
         except:
-            return False
+            return None
 
-    with ThreadPool(10) as pool:
+    with ThreadPool(20) as pool:
         results = pool.map(is_proxy_working, proxy_arr)
 
-    working_proxies = [proxy for proxy, is_working in zip(proxy_arr, results) if is_working]
-    print(f"Found {len(working_proxies)} working proxies")
-    return working_proxies
+    working_html = [result for result in results if result]
+    print(f"Found {len(working_html)} working proxies with valid HTML")
+    return working_html
     
-
+def get_proxyscrape_proxies():
+    proxies = requests.get("https://api.proxyscrape.com/v4/free-proxy-list/get?request=display_proxies&country=bd&proxy_format=protocolipport&format=text").text.split("\n")
+    proxies = [proxy.strip() for proxy in proxies if proxy.strip()]
+    print(f"Found {len(proxies)} proxies")
+    print(proxies)
+    return proxies
 
 def get_nu_html():
     # using the proxies, do a get request
     proxies = get_http_proxy()
     proxies += ["socks4://" + proxy for proxy in get_socks4_proxy()]
     proxies += ["socks5://" + proxy for proxy in get_socks5_proxy()]
-    print(f"Total {len(proxies)} proxies")
 
-    headers = {
-        "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36"
-    }
+    proxies += get_proxyscrape_proxies()
+
+    print(f"Total {len(proxies)} proxies")
 
     retry = 0
 
     # proxies = check_proxies(proxies)
 
+    html = []
+
     while retry <= 3:
-        for i, proxy in enumerate(proxies):
-            try:
-                response = requests.get("https://www.nu.ac.bd/recent-news-notice.php", headers=headers, proxies={"http": proxy, "https": proxy}, timeout=5)
-                print(response.status_code)
-                # save response to file
-                with open(f"response_{i}.html", "w", encoding="utf-8") as f:
-                    f.write(response.text)
-                soup = BeautifulSoup(response.text, 'html.parser')
-                check_nu_working()
-                retry = 4
-                return soup
-            except Exception as e:
-                print(traceback.format_exc())
-                print(f"Failed to connect using {proxy}. Error: {e}")
-                continue
-        retry += 1
+        html = check_proxies(proxies)
+        print(f"Got {len(html)} htmls")
+        if len(html) > 0:
+            soup = BeautifulSoup(html[0], 'html.parser')
+            return soup
+        else:
+            retry += 1
 
 if __name__ == "__main__":
     get_nu_html()
